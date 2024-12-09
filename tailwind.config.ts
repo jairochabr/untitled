6a623530261e5574eb1ae9cc240f73e3df92c20f
Jairo Rocha <jairochabr@gmail.com>
2024-12-08 21:30:17 -0300

    chore: created the initial structure of the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,14 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/**/*.tsx",
-  ],
+  content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        app: "minmax(18rem, 20rem) 1fr",
+      },
       colors: {
-        primary: '#7C3AED'
+        primary: "#7C3AED",
       },
     },
   },
